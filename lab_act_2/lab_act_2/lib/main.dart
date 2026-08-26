9fb741f2a18d23a109374c85fdd2dbb351b54ed4
LAB12_USER ON WS26 <L12X16W26@LabHCI.net>
2026-08-26 16:42:43 +0800

    implement roll dice

--- lab_act_2/lab_act_2/lib/main.dart
+++ lab_act_2/lab_act_2/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:lab_act_2/dice_roller.dart';
+
 
 void main() {
   runApp(
@@ -12,21 +14,7 @@ void main() {
             ])
           ),
           child: Center(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Image.asset(
-                  width:200,
-                  'assets/dice-images/dice-images/dice-1.png'),
-                  SizedBox(height: 20),
-                TextButton(
-                  onPressed: () {},
-                  child: Text(
-                    style: TextStyle(
-                      fontSize: 28
-                    ),
-                    "Roll Dice"))
-            ],)
+            child: DiceRoller()
             ),
         ),
       ),
